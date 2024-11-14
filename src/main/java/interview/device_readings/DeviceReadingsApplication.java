b22ca1cd6ddf72785bcc9ed0ded46ee5ea1d1f69
CorvusCloudburst <halite.ann@gmail.com>
2024-11-14 10:50:51 -0500

    init

---
+++ src/main/java/interview/device_readings/DeviceReadingsApplication.java
@@ -0,0 +1,13 @@
+package interview.device_readings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceReadingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceReadingsApplication.class, args);
+	}
+
+}
